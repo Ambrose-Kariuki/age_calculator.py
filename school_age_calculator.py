1b4ba97329f841efc38e8e25e1694a91e53d3b74
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-03-27 17:40:23 +0300

    school age calculator intial commit

---
+++ school_age_calculator.py
@@ -0,0 +1,12 @@
+def school_age_calculator(age,name):
+    age = int(age)
+    if age < 6:
+        print(f"Enjoy your childhood, {name} is only {age} years old")
+
+    elif age == 6:
+        print("Welcome to school, {name}, is oly {age} years old")
+    else:
+        print("You are a big kid, {name},is only {age} years old")
+
+
+school_age_calculator(5, "John")
